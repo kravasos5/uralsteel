64823996517407f84c6643cd2cd6f1f653920b35
Vladyslav Kravchenko <kravasos18@gmail.com>
2023-11-26 13:24:47 +0500

    On branch pages №2 Изменения: - Добавлен в layouts базовый шаблон для страниц кранов и ковшей schema.html (schema.css также добавлен). 	1. Добавлена страница ковшей ladles.html (ladles.js также добавлен); 	2. Добавлена страница кранов cranes.html (cranes.js также добавлен); - Добавлен в layouts базовый шаблон для страниц сообщений info_message.html (info_message.css также добавлен). 	1. Добавлена страница access_denied.html, которая сообщает, что пользователю отказано в доступе; 	2. Добавлена страница password_recover_starting.html, которая сообщает о начале восстановления пароля; 	3. Добавлена страница password_recover_done.html, которая сообщает об успешном восстановлении пароля; - Добавлен файл functions.js для подмешивания общих для всех файлов функций, например функции-обёртки, которая проверяет авторизован ли пользователь (она пока не написана, но в будущих коммитах появится). - Добавлен файл csrf_html.html в scripts, который добавит csrf на страницы, где он нужен. - Обновлены файлы стилей basic.css, main.css, footer.css, изменены файлы header.html, main.html (добавлены ссылки на новые страницы и исправлена опечатка с двойным повторением <div class="content">)

--- uralsteel/visual/urls.py
+++ uralsteel/visual/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from visual.views import *
 
 urlpatterns = [
-    path('main/', Main.as_view(), name='main'),
+    path('', Main.as_view(), name='main'),
+    path('ladles/', Ladles.as_view(), name='ladles'),
+    path('cranes/', Cranes.as_view(), name='cranes'),
+    path('access-denied/', AccessDenied.as_view(), name='access-denied'),
 ]
--- uralsteel/visual/views.py
+++ uralsteel/visual/views.py
@@ -5,3 +5,15 @@ from django.views.generic import TemplateView
 class Main(TemplateView):
     '''Представление главной страницы'''
     template_name = 'visual/main.html'
+
+class Ladles(TemplateView):
+    '''Представление страницы с ковшами'''
+    template_name = 'visual/ladles.html'
+
+class Cranes(TemplateView):
+    '''Представление страницы с кранами'''
+    template_name = 'visual/cranes.html'
+
+class AccessDenied(TemplateView):
+    '''Представление страницы с кранами'''
+    template_name = 'visual/access_denied.html'
