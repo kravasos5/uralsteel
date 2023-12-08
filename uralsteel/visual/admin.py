82284fb4e0b7f43a342cdad3d2d1e647d252fffd
Viktor Korsakov <exnonameboy@gmail.com>
2023-12-08 20:17:37 +0500

    Изменения:  - Созданы модели групп агрегаторов  - Создана модель маршрутов  - Модели групп агрегаторов и маршрутов зарегистрированы в админпанели

--- uralsteel/visual/admin.py
+++ uralsteel/visual/admin.py
@@ -19,11 +19,16 @@ class EmployeesAdmin(admin.ModelAdmin):
 class AggregatesAdmin(admin.ModelAdmin):
     '''Редактор агрегатов'''
     list_display = ('__str__',)
-    search_fields = ('name',)
-    fields = (('name', 'num_agg', 'num_pos'),
+    search_fields = ('title',)
+    fields = (('title', 'num_agg', 'num_pos'),
               ('coord_x', 'coord_y'), 'is_broken',
               'stay_time',
               'photo')
+
+class RoutesAdmin(admin.ModelAdmin):
+    '''Редактор маршрутов'''
+    list_display = ('__str__',)
+    fields = ('aggregate_1', 'aggregate_2', 'aggregate_3', 'aggregate_4')
 
 class CranesAdmin(admin.ModelAdmin):
     '''Редактор кранов'''
@@ -36,20 +41,20 @@ class CranesAdmin(admin.ModelAdmin):
 class LadlesAdmin(admin.ModelAdmin):
     '''Редактор кранов'''
     list_display = ('__str__',)
-    search_fields = ('name',)
-    fields = ('name', 'is_active', 'is_broken')
+    search_fields = ('title',)
+    fields = ('title', 'is_active', 'is_broken')
 
 class BrandSteelAdmin(admin.ModelAdmin):
     '''Редактор марок стали'''
     list_display = ('__str__',)
-    search_fields = ('name',)
-    fields = ('name',)
+    search_fields = ('title',)
+    fields = ('title',)
 
 class DynamicTableAdmin(admin.ModelAdmin):
     '''Редактор динамической таблицы'''
     list_display = ('__str__',)
-    search_fields = ('ladle', 'num_melt', 'brand_steel', 'aggregate')
-    fields = (('ladle', 'num_melt', 'brand_steel', 'aggregate'),
+    search_fields = ('ladle', 'num_melt', 'brand_steel', 'route', 'aggregate')
+    fields = (('ladle', 'num_melt', 'brand_steel', 'route', 'aggregate'),
               ('plan_start', 'plan_end'),
               ('actual_start', 'actual_end'))
 
@@ -62,10 +67,14 @@ class AccidentAdmin(admin.ModelAdmin):
 
 admin.site.register(Employees, EmployeesAdmin)
 admin.site.register(Aggregates, AggregatesAdmin)
+admin.site.register(AggregatesGMP, AggregatesAdmin)
+admin.site.register(AggregatesUKP, AggregatesAdmin)
+admin.site.register(AggregatesUVS, AggregatesAdmin)
+admin.site.register(AggregatesMNLZ, AggregatesAdmin)
+admin.site.register(Routes, RoutesAdmin)
 admin.site.register(Cranes, CranesAdmin)
 admin.site.register(Ladles, LadlesAdmin)
 admin.site.register(BrandSteel, BrandSteelAdmin)
-admin.site.register(DynamicTable, DynamicTableAdmin)
 admin.site.register(ArchiveDynamicTable, DynamicTableAdmin)
 admin.site.register(ActiveDynamicTable, DynamicTableAdmin)
 admin.site.register(LadlesAccident, AccidentAdmin)
